0b43993e3be32911ae127845dcb230d3467bebcc
Cuong Nguyen <cuongnnc2610@gmail.com>
2018-09-16 20:23:45 +0700

    first commit

---
+++ app/src/test/java/com/class16t1/dominhthuy/problem3sociss/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.class16t1.dominhthuy.problem3sociss;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
